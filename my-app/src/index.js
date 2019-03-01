ab1bdd42644f728e59071950c620990029680060
Katherine Piniol <piniolk@hawaii.edu>
2019-02-28 18:14:54 -1000

    task 2

---
+++ my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+class SyndicateReplication extends React.Component {
+
+  render() {
+    return (
+        <h1>Syndicate Replication</h1>
+  );
+  }
+}
+
+ReactDOM.render(<SyndicateReplication />, document.getElementById('root'));
